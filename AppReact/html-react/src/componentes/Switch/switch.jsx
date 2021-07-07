48d720046238cd3bf3812e2cf6b273acdad84075
Alam Ferrera <alamferrera@gmail.com>
2021-07-07 16:47:11 -0400

    Dark mode agregado
    dark mode agregado

--- AppReact/html-react/src/componentes/Switch/switch.jsx
+++ AppReact/html-react/src/componentes/Switch/switch.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './switch.css';
 
 function Switch(){
+    const ref = useRef(null);
 
-    function handleChange(e){
-        console.log(e);
+    function handleChange(){
+        if(ref.current.checked){
+            document.body.classList.remove('is-light-mode');
+            document.body.classList.add('is-dark-mode');
+        }
+        else{
+            document.body.classList.remove('is-dark-mode');
+            document.body.classList.add('is-light-mode');
+        }
     }
 
     return(
         <div className="dark-mode" >
         <p className="dark-mode-title">Dark Mode</p>
-        <input type="checkbox" className="checkbox" id="checkbox" onChange={handleChange}/>
+        <input type="checkbox" className="checkbox" id="checkbox" ref={ref} onChange={handleChange}/>
         <label className="switch" htmlFor="checkbox"/>
         </div>
     );
